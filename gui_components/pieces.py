319c7220ebfbbdeb01ba61efb58dd6ea95c9d3d3
prince <nguhprince1@gmail.com>
2022-11-07 11:32:22 +0100

    Added promotion method to Piece class

--- gui_components/pieces.py
+++ gui_components/pieces.py
@@ -23,7 +23,7 @@ colors_notations_and_values = {
 class Piece():
     def __init__(self, name, notation, color, skin_directory="skins/default", value: int=None) -> None:
         self.name = name
-        self.notation = notation
+        self.__notation = notation
         self.color = color
         self.skin_directory = skin_directory
 
@@ -33,7 +33,7 @@ class Piece():
             self.value = self.get_piece_value()
     
     def get_piece_value(self):
-        return colors_notations_and_values[self.color][self.notation.lower()]
+        return colors_notations_and_values[self.color][self.__notation.lower()]
 
     def get_piece_color_based_on_notation(notation) -> str:
         return "w" if notation.isupper() else "b"
@@ -42,7 +42,7 @@ class Piece():
         return colors_notations_and_values[color][notation.lower()]
 
     def get_image_path(self):
-        return os.path.join(self.skin_directory, self.color, f"{self.notation.lower()}.png")
+        return os.path.join(self.skin_directory, self.color, f"{self.__notation.lower()}.png")
 
     def get_image(self):
         image_path = self.get_image_path()
@@ -53,13 +53,29 @@ class Piece():
             raise FileNotFoundError(f"The image was not found in the {image_path}")
 
     def __str__(self):
-        return f"{self.notation} {self.color}"
+        return f"{self.__notation} {self.color}"
 
     def get_notation(self) -> str:
-        if self.notation != 'p':
-            return self.notation.upper()
+        if self.__notation != 'p':
+            return self.__notation.upper()
 
         return ''
+
+    def __set_notation(self, notation):
+        self.__notation = notation
+
+    def promote(self, notation: str):
+        """
+        Promotes this piece to a piece with the notation
+        """
+        if self.__notation.lower() != "p":
+            raise ValueError("Cannot promote a piece other than a pawn")
+        
+        if notation not in ["q", "r", "n", "b"]:
+            raise ValueError("Can only promote to queen, rook, bishop or knight pieces")
+
+        self.__set_notation(notation)
+        
 
 class Pawn(Piece):
     def __init__(self, color, skin_directory) -> None:
